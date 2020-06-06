aca7aa0ffef72aad2fe1ca22d0b132f0ac4a535d
Goran Grbic <tpojka@gmail.com>
2020-06-06 18:55:01 +0200

    Overriden handle method to get more friendly info output about class has been created. This way you don't need to think twice what/where/why because now instead
    Class created successfully.
    
    you get message like
    
    App\Service\UserService created successfully.

--- src/Console/MakeClassCommand.php
+++ src/Console/MakeClassCommand.php
@@ -3,6 +3,7 @@
 namespace Hisman\MakeClass\Console;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputOption;
 
 class MakeClassCommand extends GeneratorCommand
@@ -66,5 +67,48 @@ class MakeClassCommand extends GeneratorCommand
             ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the class already exists'],
             ['constructor', 'c', InputOption::VALUE_NONE, 'Create a new class with constructor'],
         ];
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return bool|null
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function handle()
+    {
+        // First we need to ensure that the given name is not a reserved word within the PHP
+        // language and that the class name will actually be valid. If it is not valid we
+        // can error now and prevent from polluting the filesystem using invalid files.
+        if ($this->isReservedName($this->getNameInput())) {
+            $this->error('The name "'.$this->getNameInput().'" is reserved by PHP.');
+
+            return false;
+        }
+
+        $name = $this->qualifyClass($this->getNameInput());
+
+        $path = $this->getPath($name);
+
+        // Next, We will check to see if the class already exists. If it does, we don't want
+        // to create the class and overwrite the user's code. So, we will bail out so the
+        // code is untouched. Otherwise, we will continue generating this class' files.
+        if ((! $this->hasOption('force') ||
+                ! $this->option('force')) &&
+            $this->alreadyExists($this->getNameInput())) {
+            $this->error($name.' already exists!');
+
+            return false;
+        }
+
+        // Next, we will generate the path to the location where this class' file should get
+        // written. Then, we will build the class and make the proper replacements on the
+        // stub files so that it gets the correctly formatted namespace and class name.
+        $this->makeDirectory($path);
+
+        $this->files->put($path, $this->sortImports($this->buildClass($name)));
+
+        $this->info($name.' created successfully.');
     }
 }
